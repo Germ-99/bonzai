c1e71a34aa1a45edf86c60f73d47b47ca4c0b014
Hollow- <107813339+hollowdash@users.noreply.github.com>
2025-08-26 09:28:57 -0500

    Update SettingsMenu.cs

--- Bonzai/SettingsMenu.cs
+++ Bonzai/SettingsMenu.cs
@@ -50,54 +50,37 @@ namespace Bonzai
             Properties.Settings.Default.lastGoal = LGCHECK.Checked;
             Properties.Settings.Default.regrabSpeed = RSCHECK.Checked;
             Properties.Settings.Default.pingSpike = PSCHECK.Checked;
-
             Properties.Settings.Default.Save();
 
-            if (Properties.Settings.Default.voiceGender == 0) 
-            { 
-                MainMenu.synthesizer.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult); 
+            switch(Properties.Settings.Default.voiceGender){
+                case: 0{
+                    MainMenu.synthesizer.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult); 
+                    break;
+                }
+                case: 1{
+                    MainMenu.synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult); 
+                    break;
+                }
             }
-            else if (Properties.Settings.Default.voiceGender == 1) 
-            { 
-                MainMenu.synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult); 
+            switch(Properties.Settings.Default.voiceSpeed){
+                case: 0{
+                    MainMenu.synthesizer.Rate = -5;
+                    FetchAPIData.synthesizer.Rate = -5; 
+                    break;
+                }
+                case: 1{
+                    MainMenu.synthesizer.Rate = 0;
+                    FetchAPIData.synthesizer.Rate = 0; 
+                    break;
+                }
+                case: 2{
+                    MainMenu.synthesizer.Rate = 7;
+                    FetchAPIData.synthesizer.Rate = 7; 
+                    break;
+                }
             }
-
-            if (Properties.Settings.Default.voiceSpeed == 0) 
-            { 
-                MainMenu.synthesizer.Rate = -5; 
-            }
-            else if (Properties.Settings.Default.voiceSpeed == 1) 
-            { 
-                MainMenu.synthesizer.Rate = 0; 
-            }
-            else if (Properties.Settings.Default.voiceSpeed == 2) 
-            { 
-                MainMenu.synthesizer.Rate = 7; 
-            }
-
-            if (Properties.Settings.Default.voiceGender == 0) 
-            { 
-                FetchAPIData.synthesizer.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult); 
-            }
-            else if (Properties.Settings.Default.voiceGender == 1) 
-            { 
-                FetchAPIData.synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult); 
-            }
-
-            if (Properties.Settings.Default.voiceSpeed == 0) 
-            { 
-                FetchAPIData.synthesizer.Rate = -5; 
-            }
-            else if (Properties.Settings.Default.voiceSpeed == 1) 
-            { 
-                FetchAPIData.synthesizer.Rate = 0; 
-            }
-            else if (Properties.Settings.Default.voiceSpeed == 2) 
-            { 
-                FetchAPIData.synthesizer.Rate = 7; 
-            }
-
-            System.Diagnostics.Debug.WriteLine(Properties.Settings.Default.voiceGender.ToString() + " + " + Properties.Settings.Default.voiceSpeed.ToString());
+            // for testing
+            // System.Diagnostics.Debug.WriteLine(Properties.Settings.Default.voiceGender.ToString() + " + " + Properties.Settings.Default.voiceSpeed.ToString());
         }
 
         private void TTSCHECK_CheckedChanged(object sender, EventArgs e)
