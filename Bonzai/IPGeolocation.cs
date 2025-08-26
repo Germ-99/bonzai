8f275f21c06b737dc70490bf02b7c9ab9cb62c1b
Hollow- <107813339+hollowdash@users.noreply.github.com>
2025-08-26 09:21:48 -0500

    Update IPGeolocation.cs

--- Bonzai/IPGeolocation.cs
+++ Bonzai/IPGeolocation.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 public class IPGeolocation
 {
-    // This method retrieves the city and state from an IP address
+
     public static async Task<string> GetLocationAsync(string ipAddress)
     {
         string url = $"http://ip-api.com/json/{ipAddress}";
@@ -19,16 +19,12 @@ public class IPGeolocation
 
                 string responseBody = await response.Content.ReadAsStringAsync();
 
-                // Parse the JSON response using System.Text.Json
                 using (JsonDocument doc = JsonDocument.Parse(responseBody))
                 {
                     JsonElement root = doc.RootElement;
 
-                    // Extract city and region (state) from the response
                     string city = root.GetProperty("city").GetString();
                     string region = root.GetProperty("regionName").GetString();
-
-                    // Combine city and state into a single string
                     string cityState = $"{city}, {region}";
 
                     return cityState;
@@ -39,7 +35,6 @@ public class IPGeolocation
         }
         catch (HttpRequestException e)
         {
-            // Return an error message in case of a request failure
             return $"Error retrieving location: {e.Message}";
         }
     }
